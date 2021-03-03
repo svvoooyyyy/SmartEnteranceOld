1aa08ff0b38d0f4dc3d2a7ef5c8aba7d3d4ae0b2
banana_dan <glokback@gmail.com>
2021-03-03 15:13:50 +0300

    i added scanner

--- app/src/main/java/com/example/schoolhelper/TeacherMainPage.java
+++ app/src/main/java/com/example/schoolhelper/TeacherMainPage.java
@@ -1,30 +1,114 @@
 package com.example.schoolhelper;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.nfc.tech.NfcBarcode;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.pnuema.java.barcode.Barcode;
 
+import static com.example.schoolhelper.ScannerActivity.ACTION_SCAN;
+
 public class TeacherMainPage extends AppCompatActivity {
+
+    public static final int TEACHER_MAIN_PAGE_REQUEST_CODE = 12;
     ImageView activity_teacher_image_view_back;
+    Button scanner_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_main_page);
-
-
-
         activity_teacher_image_view_back = findViewById(R.id.activity_teacher_image_view_back);
+
+
         activity_teacher_image_view_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    // Запускаемм сканер штрих кода:
+    public void scanBar(View v) {
+        try {
+
+            // Запускаем переход на com.google.zxing.client.android.SCAN с помощью intent:
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+
+            // Предлагаем загрузить с Play Market:
+            showDialog(TeacherMainPage.this, "Сканнер не найден", "Установить сканер с Play Market?", "Да", "Нет").show();
+        }
+    }
+
+    // Запуск сканера qr-кода:
+    public void scanQR(View v) {
+        try {
+
+            // Запускаем переход на com.google.zxing.client.android.SCAN с помощью intent:
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+
+            // Предлагаем загрузить с Play Market:
+            showDialog(TeacherMainPage.this, "Сканнер не найден", "Установить сканер с Play Market?", "Да", "Нет").show();
+        }
+    }
+
+    // alert dialog для перехода к загрузке приложения сканера:
+    private static AlertDialog showDialog(final Activity act, CharSequence title,
+                                          CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                // Ссылка поискового запроса для загрузки приложения:
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return downloadDialog.show();
+    }
+
+    // Обрабатываем результат, полученный от приложения сканера:
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+
+                // Получаем данные после работы сканера и выводим их в Toast сообщении:
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast toast = Toast.makeText(this, "Содержание: " + contents + " Формат: " + format, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
     }
 }
